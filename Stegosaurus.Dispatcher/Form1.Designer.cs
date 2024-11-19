dd66ac5739e5ec9d9bd3aae5857cbd3eadb6c20a
alex <thegamerale97@gmail.com>
2024-11-19 20:52:13 +0100

    old code

--- Stegosaurus.Dispatcher/Form1.Designer.cs
+++ Stegosaurus.Dispatcher/Form1.Designer.cs
@@ -24,16 +24,48 @@ partial class Form1
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        btn_connect_exchange_windows = new System.Windows.Forms.Button();
+        btn_connect_exchange_linux = new System.Windows.Forms.Button();
+        SuspendLayout();
+        // 
+        // btn_connect_exchange_windows
+        // 
+        btn_connect_exchange_windows.Location = new System.Drawing.Point(116, 184);
+        btn_connect_exchange_windows.Name = "btn_connect_exchange_windows";
+        btn_connect_exchange_windows.Size = new System.Drawing.Size(126, 25);
+        btn_connect_exchange_windows.TabIndex = 0;
+        btn_connect_exchange_windows.Text = "Connect widnows";
+        btn_connect_exchange_windows.UseVisualStyleBackColor = true;
+        btn_connect_exchange_windows.Click += btn_connect_exchange_windows_Click;
+        // 
+        // btn_connect_exchange_linux
+        // 
+        btn_connect_exchange_linux.Location = new System.Drawing.Point(603, 186);
+        btn_connect_exchange_linux.Name = "btn_connect_exchange_linux";
+        btn_connect_exchange_linux.Size = new System.Drawing.Size(133, 23);
+        btn_connect_exchange_linux.TabIndex = 1;
+        btn_connect_exchange_linux.Text = "Connect linux";
+        btn_connect_exchange_linux.UseVisualStyleBackColor = true;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(btn_connect_exchange_linux);
+        Controls.Add(btn_connect_exchange_windows);
+        Text = "Form1";
+        ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Button btn_connect_exchange_windows;
+
+    private System.Windows.Forms.Button btn_connect_exchange_linux;
 
     #endregion
 }
--- Stegosaurus.Dispatcher/Form1.cs
+++ Stegosaurus.Dispatcher/Form1.cs
@@ -1,3 +1,8 @@
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RabbitMQ.Client;
+
 namespace Stegosaurus.Dispatcher;
 
 public partial class Form1 : Form
@@ -5,5 +10,35 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private async void btn_connect_exchange_windows_Click(object sender, EventArgs e)
+    {
+        string msg = "";
+        var factory = new ConnectionFactory
+        {
+            HostName = "game.xela.space"
+        };
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: "Creation", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+
+        const string message = "Hello World!";
+        // read JSON directly from a file
+        using (StreamReader file = File.OpenText(@"C:\Users\thega\AppData\Roaming\StegoShard\request.json"))
+        using (JsonTextReader reader = new JsonTextReader(file))
+        {
+            JObject o2 = (JObject)JToken.ReadFrom(reader);
+            msg = o2.ToString();
+        }
+
+        var body = Encoding.UTF8.GetBytes(msg);
+
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "Creation", body: body);
+        Console.WriteLine($" [x] Sent {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
     }
 }
