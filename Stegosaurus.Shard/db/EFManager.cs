8246b660ec5595b32f5fb71e39841d51213b3f54
xela <thegamerale97@gmail.com>
2024-12-12 12:25:06 +0100

    COnfiguring entity framework

---
+++ Stegosaurus.Shard/db/EFManager.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
+namespace Stegosaurus.Shard.db;
+
+public class EFManagerGameServers : DbContext
+{
+    public DbSet<gameservers>  Gameservers { get; set; }
+    
+    static string connectionString = "server=game.xela.space;user=xela;password=antD)nIo3]dKvp7K;database=Development";
+    MariaDbServerVersion serverVersion = new MariaDbServerVersion(ServerVersion.AutoDetect(connectionString));
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(connectionString,serverVersion).LogTo(Console.WriteLine, LogLevel.Debug)
+            .EnableSensitiveDataLogging().EnableDetailedErrors();
+    }
+}
+
+
+public class gameservers
+{
+    public int id { get; set; }
+    public string shard_id { get; set; }
+    public string container_id { get; set; }
+    public status status { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+}
+
+public enum status
+{
+    running,
+    stopped,
+    error
+}
