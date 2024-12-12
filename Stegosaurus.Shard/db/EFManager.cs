4b9789cd5685427eea8796f913ffbca5c07c0d41
xela <thegamerale97@gmail.com>
2024-12-12 12:54:21 +0100

    Im about to rm -rf / --no-preserve-root myself

--- Stegosaurus.Shard/db/EFManager.cs
+++ Stegosaurus.Shard/db/EFManager.cs
@@ -24,6 +24,7 @@ public class gameservers
     public int id { get; set; }
     public string shard_id { get; set; }
     public string container_id { get; set; }
+    
     public status status { get; set; }
     public DateTime created_at { get; set; }
     public DateTime updated_at { get; set; }
